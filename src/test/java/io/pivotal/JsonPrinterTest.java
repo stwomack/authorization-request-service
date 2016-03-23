66830e2b20615d0212c4e6ab3e2187e7e009a1d6
Steve Womack <stwomack@gmail.com>
2016-03-23 17:29:57 -0500

    Adds better randomization for data creation. Can later use the test to do HTTP Posts to actual endpoint

---
+++ src/test/java/io/pivotal/JsonPrinterTest.java
@@ -0,0 +1,36 @@
+package io.pivotal;
+
+import io.pivotal.domain.AuthorizationRequest;
+import kafka.utils.Json;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Random;
+
+public class JsonPrinterTest {
+
+    @Test
+    public void printIt() throws IOException {
+        RandomStringUtils randomStringUtils = new RandomStringUtils();
+
+        ObjectMapper mapper = new ObjectMapper();
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(
+                randLong(), randLong(), randLong(), randLong(),
+                Math.abs(new Random().nextDouble()), randomStringUtils.randomAlphabetic(8), new Date());
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(authorizationRequest.toString()));
+    }
+
+    private long randLong() {
+        Long min = new Long(10000000);
+        Long max = new Long(99999999);
+
+        return Math.abs(randomLong(min, max));
+    }
+
+    private long randomLong(long min, long max) {
+        return (new java.util.Random().nextLong() % (max - min)) + min;
+    }
+}
